5f22b088059db3a8bfe5e851fdc5597610973ad1
Vitalii L <vitalii.lukianenko@gmail.com>
2025-09-24 15:26:42 +0300

    add cors commit

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ resource "aws_s3_bucket_cors_configuration" "this" {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
     allowed_origins = ["https://${var.project_domain}"]
-    expose_headers  = ["*"]
     max_age_seconds = 3000
   }
 }
